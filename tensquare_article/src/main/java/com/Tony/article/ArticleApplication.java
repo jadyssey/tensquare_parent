49cce908c1a76355df69e8ab785583d563abf55d
HugoCity <liu9801@foxmail.com>
2020-04-12 18:34:23 +0800

    (2)框架完善-文章微服务搭建

---
+++ tensquare_article/src/main/java/com/Tony/article/ArticleApplication.java
@@ -0,0 +1,21 @@
+package com.Tony.article;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author AntonTony
+ * @version 1.0
+ * @GitHub https://github.com/AntonTony
+ */
+
+//spring启动类
+@SpringBootApplication
+public class ArticleApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(ArticleApplication.class,args);
+    }
+
+}
+
