91c12ee2f1e04d6a14d09dbc37bbaf2417410fb7
HugoCity <liu9801@foxmail.com>
2020-04-17 13:19:44 +0800

    Zuul网关实现解密和转发

--- tensquare_encrypt/src/main/java/com/Tony/encrypt/filter/RSARequestFilter.java
+++ tensquare_encrypt/src/main/java/com/Tony/encrypt/filter/RSARequestFilter.java
@@ -1,9 +1,24 @@
 package com.Tony.encrypt.filter;
 
+import com.Tony.encrypt.rsa.RsaKeys;
+import com.Tony.encrypt.service.RsaService;
+import com.google.common.base.Charsets;
+import com.google.common.base.Strings;
 import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
+import com.netflix.zuul.http.ServletInputStreamWrapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StreamUtils;
+
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * @author AntonTony
@@ -14,6 +29,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class RSARequestFilter extends ZuulFilter {
 
+    @Autowired
+    private RsaService rsaService; //注入rsaService用于调取解密函数
 
     @Override
     public String filterType() {
@@ -35,7 +52,68 @@ public class RSARequestFilter extends ZuulFilter {
 
     @Override
     public Object run() throws ZuulException {
+        /**
+         * 1. 前端用公钥加密，然后通过请求体传给后端
+         * 2. 从request body中读取出加密后的请求参数
+         * 3. 将加密后的请求参数用私钥解密
+         * 4. 将解密后的请求参数写回request body中
+         * 5. 转发请求
+         */
         System.out.println("zuulException执行了");
+
+        //获取RequestContext容器 拿到Request请求和Response响应
+        RequestContext ctx = RequestContext.getCurrentContext();
+
+        //通过容器获得Request和Response请求响应
+        HttpServletRequest request = ctx.getRequest();
+        HttpServletResponse response = ctx.getResponse();
+
+
+        String requestData = null;//声明存放加密后的数据的变量
+        String decryptData = null;//声明存放解密后的数据变量
+
+        try {
+            //通过Request获取inputStream
+            ServletInputStream inputStream = request.getInputStream();
+
+            //从inputsteam中得到加密后的数据
+            requestData = StreamUtils.copyToString(inputStream, Charsets.UTF_8);
+            System.out.println(requestData);//输出加密后的数据
+
+            //对加密后的数据进行解密操作
+            if(!Strings.isNullOrEmpty(requestData)){
+               decryptData = rsaService.RSADecryptDataPEM(requestData, RsaKeys.getServerPrvKeyPkcs8());
+                System.out.println(decryptData);
+            }
+
+            if(!Strings.isNullOrEmpty(decryptData)) {
+                System.out.println("json字符串写入request body");
+                final byte[] reqBodyBytes = decryptData.getBytes();
+                ctx.setRequest(new HttpServletRequestWrapper(request) {
+                    @Override
+                    public ServletInputStream getInputStream() throws IOException {
+                        return new ServletInputStreamWrapper(reqBodyBytes);
+                    }
+
+                    @Override
+                    public int getContentLength() {
+                        return reqBodyBytes.length;
+                    }
+
+                    @Override
+                    public long getContentLengthLong() {
+                        return reqBodyBytes.length;
+                    }
+                });
+            }
+
+            // 设置request请求头中的Content-Type为application/json，否则api接口模块需要进行url转码操作
+            ctx.addZuulRequestHeader("Content-Type", String.valueOf(MediaType.APPLICATION_JSON) + ";charset=UTF-8");
+        } catch (Exception e) {
+            System.out.println(this.getClass().getName() + "运行出错" + e.getMessage());
+        }
         return null;
     }
+
+
 }
--- tensquare_encrypt/src/test/java/EncryptTest.java
+++ tensquare_encrypt/src/test/java/EncryptTest.java
@@ -25,12 +25,13 @@ public class EncryptTest {
     public void after() throws Exception {
     }
 
+//    测试加密方法
     @Test
     public void genEncryptDataByPubKey() {
-        String data = "{\"title\":\"java\"}";
+        String data = "{\"title\":\"java\"}"; //被加密的Json数据
 
         try {
-
+            //RSAEncryptDataPEM是使用Rsa加密方法，getServerPubKey为获取公钥
             String encData = rsaService.RSAEncryptDataPEM(data, RsaKeys.getServerPubKey());
 
             System.out.println("data: " + data);
@@ -39,4 +40,17 @@ public class EncryptTest {
             e.printStackTrace();
         }
     }
+
+//    测试解密方法
+    @Test
+    public void test() throws Exception {
+//        在控制台获取的被加密后的数据作为输入
+        String requestData = "XQ30MH5XHmycFUCJNKixuoDXMBgI7J/TYKYHXCql8mEO/WnhYGS/altTdcIGkE2rd/7sJptk5uIBso0PKjPEjXZ0CLetXjiCCsrhOg2xuJSSoqnQdNPasWu+Y+IVxJw2kK52hpTH/f/rsWQK0EQKNx0DK/UO/hStTJMlactB3TclFGmjtpXX9/UELInEWhNmFS39eCuUpLp0VvLOJeNRadaIhiIO3Qu4g9j4+Cqzk9Qi+cHw2bDzrWCBGTaKOHURDpeaVA7RX5/VkeOsRB03mKYwv49xUN/fIafsmHO82WzOA4Cb1tzpb6Urrjx8vVOCq97aRUHaT4/fpCMFWfb2Dw==";
+//        RSADecryptDataPEM是解密方法，getServerPrvKeyPkcs8获取私钥
+        String decryptData = rsaService.RSADecryptDataPEM(requestData, RsaKeys.getServerPrvKeyPkcs8());
+//        输出解密数据
+        System.out.println(decryptData);
+
+    }
+
 }
